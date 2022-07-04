6d95447ddb70df7790dd3618d86f5fa9414766f5
Qumansa <qumansa@yandex.ru>
2022-07-04 14:31:52 +0400

    Подключение базовых стилей и шрифтов
    + создание шаблона для хедера

---
+++ src/components/app/App.js
@@ -0,0 +1,13 @@
+import Header from '../header/Header';
+
+import '../../style/_base.sass';
+
+function App() {
+  	return (
+    	<>
+			<Header/>
+    	</>
+  	);
+}
+
+export default App;
---
+++ src/components/header/Header.js
@@ -0,0 +1,13 @@
+import './header.sass';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <div className="container">
+                Хедер
+            </div>
+        </header>
+    );
+};
+
+export default Header;

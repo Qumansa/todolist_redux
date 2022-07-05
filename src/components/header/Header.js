03ea8b57d852bf783df0e689d31c64a331eeec9c
Qumansa <qumansa@yandex.ru>
2022-07-05 13:41:40 +0400

    Хедер
    Верстка

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,9 +2,9 @@ import './header.sass';
 
 const Header = () => {
     return (
-        <header className='header'>
+        <header className="header">
             <div className="container">
-                Хедер
+                <h1 className="header__title">To-Do List</h1>
             </div>
         </header>
     );

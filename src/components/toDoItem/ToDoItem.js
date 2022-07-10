737cadbe85833a841f5600ef82b9bd251c87a1d0
Qumansa <qumansa@yandex.ru>
2022-07-10 13:18:34 +0400

    Редактирование задачи
    Создание возможности переключаться на режим редактирования

--- src/components/toDoItem/ToDoItem.js
+++ src/components/toDoItem/ToDoItem.js
@@ -1,13 +1,59 @@
+import { useState } from 'react';
+
 import './toDoItem.sass';
 
 const ToDoItem = ({index, onDelete, onToggle, description, favourite}) => {
+    const [beingEdited, setBeingEdited] = useState(false);
+
     const btnActiveClass = favourite ? 'to-do-list__button_active' : null;
+
+    const descriptionElem = (
+        <span className="to-do-list__description">{description}</span>
+    );
+
+    const inputElem = (
+        <input 
+            type="text" 
+            className="input"
+            value={description}/>
+    );
+
+    const onEdit = () => {
+        setBeingEdited(beingEdited => beingEdited = !beingEdited);
+    };
 
     return (
         <li className="to-do-list__item">
-            <span className="to-do-list__description">{index + 1}) {description}</span>
-            {/* <input type="text" className="input" /> */}
+            <div className="to-do-list__task">
+                <span className="to-do-list__order">{index + 1})</span>
+                {beingEdited ? inputElem : descriptionElem}
+            </div>
             <ul className="to-do-list__buttons">
+                <li className="to-do-list__buttons-item">
+                    <button
+                        className={`to-do-list__button ${btnActiveClass}`}
+                        aria-label="Edit"
+                        onClick={onEdit}>
+                        <svg className="to-do-list__button-img" version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 490.337 490.337">
+                            <g>
+                                <g>
+                                    <path d="M229.9,145.379l-47.5,47.5c-17.5,17.5-35.1,35-52.5,52.7c-4.1,4.2-7.2,9.8-8.4,15.3c-6.3,28.9-12.4,57.8-18.5,86.7
+                                        l-3.4,16c-1.6,7.8,0.5,15.6,5.8,20.9c4.1,4.1,9.8,6.4,15.8,6.4c1.7,0,3.4-0.2,5.1-0.5l17.6-3.7c28-5.9,56.1-11.9,84.1-17.7
+                                        c6.5-1.4,12-4.3,16.7-9c78.6-78.7,157.2-157.3,235.8-235.8c5.8-5.8,9-12.7,9.8-21.2c0.1-1.4,0-2.8-0.3-4.1c-0.5-2-0.9-4.1-1.4-6.1
+                                        c-1.1-5.1-2.3-10.9-4.7-16.5l0,0c-14.7-33.6-39.1-57.6-72.5-71.1c-6.7-2.7-13.8-3.6-20-4.4l-1.7-0.2c-9-1.1-17.2,1.9-24.3,9.1
+                                        C320.4,54.879,275.1,100.179,229.9,145.379z M386.4,24.679c0.2,0,0.3,0,0.5,0l1.7,0.2c5.2,0.6,10,1.2,13.8,2.8
+                                        c27.2,11,47.2,30.6,59.3,58.2c1.4,3.2,2.3,7.3,3.2,11.6c0.3,1.6,0.7,3.2,1,4.8c-0.4,1.8-1.1,3-2.5,4.3
+                                        c-78.7,78.5-157.3,157.2-235.9,235.8c-1.3,1.3-2.5,1.9-4.3,2.3c-28.1,5.9-56.1,11.8-84.2,17.7l-14.8,3.1l2.8-13.1
+                                        c6.1-28.8,12.2-57.7,18.4-86.5c0.2-0.9,1-2.3,1.9-3.3c17.4-17.6,34.8-35.1,52.3-52.5l47.5-47.5c45.3-45.3,90.6-90.6,135.8-136
+                                        C384.8,24.979,385.7,24.679,386.4,24.679z"/>
+                                    <path d="M38.9,109.379h174.6c6.8,0,12.3-5.5,12.3-12.3s-5.5-12.3-12.3-12.3H38.9c-21.5,0-38.9,17.5-38.9,38.9v327.4
+                                        c0,21.5,17.5,38.9,38.9,38.9h327.3c21.5,0,38.9-17.5,38.9-38.9v-167.5c0-6.8-5.5-12.3-12.3-12.3s-12.3,5.5-12.3,12.3v167.5
+                                        c0,7.9-6.5,14.4-14.4,14.4H38.9c-7.9,0-14.4-6.5-14.4-14.4v-327.3C24.5,115.879,31,109.379,38.9,109.379z"/>
+                                </g>
+                            </g>
+                        </svg>
+                    </button>
+                </li>
                 <li className="to-do-list__buttons-item">
                     <button
                         className={`to-do-list__button ${btnActiveClass}`}
@@ -23,7 +69,9 @@ const ToDoItem = ({index, onDelete, onToggle, description, favourite}) => {
                         className="to-do-list__button" 
                         aria-label="Delete"
                         onClick={onDelete}>
-                        <svg className="to-do-list__button-img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 128"><path d="M24,41h77v63c0,9.37-7.63,17-17,17H44c-9.37,0-17-7.63-17-17V52c0-1.66-1.34-3-3-3s-3,1.34-3,3v52c0,12.68,10.32,23,23,23 h40c12.68,0,23-10.32,23-23V40.64c5.72-1.36,10-6.5,10-12.64c0-7.17-5.83-13-13-13H24c-7.17,0-13,5.83-13,13S16.83,41,24,41z M24,21h80c3.86,0,7,3.14,7,7s-3.14,7-7,7H24c-3.86,0-7-3.14-7-7S20.14,21,24,21z"/><path d="M49,7h30c1.66,0,3-1.34,3-3s-1.34-3-3-3H49c-1.66,0-3,1.34-3,3S47.34,7,49,7z"/><path d="M53,104V58c0-1.66-1.34-3-3-3s-3,1.34-3,3v46c0,1.66,1.34,3,3,3S53,105.66,53,104z"/><path d="M81,104V58c0-1.66-1.34-3-3-3s-3,1.34-3,3v46c0,1.66,1.34,3,3,3S81,105.66,81,104z"/></svg>
+                        <svg className="to-do-list__button-img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 128">
+                            <path d="M24,41h77v63c0,9.37-7.63,17-17,17H44c-9.37,0-17-7.63-17-17V52c0-1.66-1.34-3-3-3s-3,1.34-3,3v52c0,12.68,10.32,23,23,23 h40c12.68,0,23-10.32,23-23V40.64c5.72-1.36,10-6.5,10-12.64c0-7.17-5.83-13-13-13H24c-7.17,0-13,5.83-13,13S16.83,41,24,41z M24,21h80c3.86,0,7,3.14,7,7s-3.14,7-7,7H24c-3.86,0-7-3.14-7-7S20.14,21,24,21z"/><path d="M49,7h30c1.66,0,3-1.34,3-3s-1.34-3-3-3H49c-1.66,0-3,1.34-3,3S47.34,7,49,7z"/><path d="M53,104V58c0-1.66-1.34-3-3-3s-3,1.34-3,3v46c0,1.66,1.34,3,3,3S53,105.66,53,104z"/><path d="M81,104V58c0-1.66-1.34-3-3-3s-3,1.34-3,3v46c0,1.66,1.34,3,3,3S81,105.66,81,104z"/>
+                        </svg>
                     </button>
                 </li>
             </ul>

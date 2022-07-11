5d21669b01606cd93d43909e8c2ab31a4fa2718d
Qumansa <qumansa@yandex.ru>
2022-07-11 17:03:22 +0400

    ToDoItem
    Установка автофокуса в input при редактировании задачи

--- src/components/toDoItem/ToDoItem.js
+++ src/components/toDoItem/ToDoItem.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { useDeleteToDoItemMutation, useUpdateToDoItemMutation } from '../../api/todosApi';
 
 import './toDoItem.sass';
@@ -6,11 +6,17 @@ import './toDoItem.sass';
 const ToDoItem = ({id, index, description, favourite}) => {
     const [beingEdited, setBeingEdited] = useState(false);
     const [currentDescription, setCurrentDescription] = useState(description);
-    
+
+    const inputRef = useRef(null);
+
     const [updateToDoItem] = useUpdateToDoItemMutation();
     const [deleteToDoItem] = useDeleteToDoItemMutation();
 
     const btnActiveClass = favourite ? 'to-do-list__button_active' : null;
+
+    useEffect(() => {
+        inputRef.current && inputRef.current.focus();
+    }, [beingEdited]);
 
     const onEdit = () => {
         setBeingEdited(beingEdited => beingEdited = !beingEdited);
@@ -52,7 +58,9 @@ const ToDoItem = ({id, index, description, favourite}) => {
             type="text" 
             className="input"
             value={currentDescription}
-            onChange={(e) => setCurrentDescription(e.target.value)}/>
+            ref={inputRef}
+            onChange={(e) => setCurrentDescription(e.target.value)}
+            onKeyDown={(e) => e.key === 'Enter' && onSave(id, currentDescription)}/>
     );
 
     const editElem = (
@@ -60,7 +68,7 @@ const ToDoItem = ({id, index, description, favourite}) => {
             <button
                 className={`to-do-list__button ${btnActiveClass}`}
                 aria-label="Edit"
-                onClick={onEdit}>
+                onClick={() => onEdit()}>
                 <svg className="to-do-list__button-img" version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 490.337 490.337">
                     <g>
                         <g>
@@ -91,8 +99,7 @@ const ToDoItem = ({id, index, description, favourite}) => {
                 onClick={() => onSave(id, currentDescription)}>
                 <svg className="to-do-list__button-img" version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 480.358 480.358"  >
                     <g>
-                        <path d="M387.702,43.753L181.316,253.467l-90.84-88.044L0,258.771l183.479,177.834l296.879-301.667L387.702,43.753z
-                            M182.98,394.343L42.421,258.108l48.719-50.265l90.676,87.886L388.042,86.178l49.892,49.1L182.98,394.343z"/>
+                        <path d="M387.702,43.753L181.316,253.467l-90.84-88.044L0,258.771l183.479,177.834l296.879-301.667L387.702,43.753z M182.98,394.343L42.421,258.108l48.719-50.265l90.676,87.886L388.042,86.178l49.892,49.1L182.98,394.343z"/>
                     </g>
                 </svg>
             </button>
@@ -131,7 +138,7 @@ const ToDoItem = ({id, index, description, favourite}) => {
                     <button
                         className={`to-do-list__button ${btnActiveClass}`}
                         aria-label="Favourite"
-                        onClick={() => onToggle(id, favourite)}>
+                        onClick={() => onToggle(id, favourite, index)}>
                         <svg className="to-do-list__button-img to-do-list__button-img_favourite" viewBox="-1 0 18 15" fill="white" xmlns="http://www.w3.org/2000/svg">
                             <path d="M15.4829 5.28193L10.6864 4.59758L8.54222 0.330127C8.48365 0.213287 8.38731 0.118702 8.26829 0.0612089C7.96981 -0.0834504 7.6071 0.037099 7.45786 0.330127L5.3137 4.59758L0.517213 5.28193C0.384974 5.30047 0.26407 5.36167 0.171503 5.45441C0.0595945 5.56733 -0.0020722 5.71924 5.31712e-05 5.87678C0.00217854 6.03431 0.0679221 6.18457 0.182838 6.29454L3.65316 9.61614L2.83328 14.3064C2.81405 14.4156 2.82635 14.5278 2.86878 14.6304C2.91121 14.733 2.98207 14.8218 3.07333 14.8869C3.16459 14.952 3.27259 14.9906 3.38509 14.9985C3.4976 15.0064 3.6101 14.9831 3.70983 14.9314L8.00004 12.717L12.2902 14.9314C12.4074 14.9926 12.5434 15.013 12.6737 14.9908C13.0024 14.9352 13.2235 14.6291 13.1668 14.3064L12.3469 9.61614L15.8172 6.29454C15.9117 6.20367 15.974 6.08497 15.9929 5.95515C16.0439 5.63059 15.8135 5.33015 15.4829 5.28193Z" fill="white"></path>
                         </svg>

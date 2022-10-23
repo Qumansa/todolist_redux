81c4826b69b009e3f28d75da58120de6df340ef5
Qumansa <qumansa@yandex.ru>
2022-10-23 13:50:01 +0300

    Подготовка к деплою

--- src/api/todosApi.js
+++ src/api/todosApi.js
@@ -1,49 +1,45 @@
-import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const todosApi = createApi({
-    reducerPath: 'todosApi',
-    baseQuery: fetchBaseQuery({
-        baseUrl: 'http://localhost:3001'
-    }),
-    tagTypes: ['Todos'],
-    endpoints: (builder) => ({
-        getToDoList: builder.query({
-            query: () => '/todos',
-            providesTags: ['Todos']
-        }),
-        createToDoItem: builder.mutation({
-            query: (toDoItem) => ({
-                url: '/todos',
-                method: 'POST',
-                body: toDoItem
-            }),
-            invalidatesTags: ['Todos']
-        }),
-        updateToDoItem: builder.mutation({
-            query(data) {
-                const { id } = data;
-                
-                return {
-                    url: `/todos/${id}`,
-                    method: 'PATCH',
-                    body: data
-                }
-            },
-            invalidatesTags: ['Todos']
-        }),
-        deleteToDoItem: builder.mutation({
-            query: (id) => ({
-                url: `/todos/${id}`,
-                method: 'DELETE'
-            }),
-            invalidatesTags: ['Todos']
-        })
-    })
+	reducerPath: 'todosApi',
+	baseQuery: fetchBaseQuery({
+		baseUrl: '/',
+	}),
+	tagTypes: ['Todos'],
+	endpoints: (builder) => ({
+		getToDoList: builder.query({
+			query: () => '/todos',
+			providesTags: ['Todos'],
+		}),
+		createToDoItem: builder.mutation({
+			query: (toDoItem) => ({
+				url: '/todos',
+				method: 'POST',
+				body: toDoItem,
+			}),
+			invalidatesTags: ['Todos'],
+		}),
+		updateToDoItem: builder.mutation({
+			query(data) {
+				const { id } = data;
+
+				return {
+					url: `/todos/${id}`,
+					method: 'PATCH',
+					body: data,
+				};
+			},
+			invalidatesTags: ['Todos'],
+		}),
+		deleteToDoItem: builder.mutation({
+			query: (id) => ({
+				url: `/todos/${id}`,
+				method: 'DELETE',
+			}),
+			invalidatesTags: ['Todos'],
+		}),
+	}),
 });
 
-export const {
-    useGetToDoListQuery, 
-    useDeleteToDoItemMutation, 
-    useCreateToDoItemMutation, 
-    useUpdateToDoItemMutation
-} = todosApi;
+export const { useGetToDoListQuery, useDeleteToDoItemMutation, useCreateToDoItemMutation, useUpdateToDoItemMutation } =
+	todosApi;
